2940e1185ea0e90767aa100079cf57b1c31ec729
sahar E. Hassan <sahar.e.hassan2015@gmail.com>
2019-09-07 14:41:55 +0200

    Updates

---
+++ precache-manifest.d2c456d8f06f577cb6e41b1a7b2262b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720cc50b916afc0069e7cbcbfaf781e1",
+    "url": "/Blogs-ReactRedux/index.html"
+  },
+  {
+    "revision": "dc73095f1445b735fd93",
+    "url": "/Blogs-ReactRedux/static/js/2.da63ebe0.chunk.js"
+  },
+  {
+    "revision": "bb7804d53484b96d76cb",
+    "url": "/Blogs-ReactRedux/static/js/main.7bd5e561.chunk.js"
+  },
+  {
+    "revision": "e3cadc5d18b6756f7a8e",
+    "url": "/Blogs-ReactRedux/static/js/runtime~main.1821e8a5.js"
+  }
+]);
